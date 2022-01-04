7b1097dfd57bb7fa989e3c9e829ad9634852e8bb
PandaLord <330213905@qq.com>
2022-01-05 00:10:26 +0800

    settle package for project

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 pub mod trigger;
 pub mod data;
 
+
+use sled;
 fn main() {
+    let db : sled::Db  = sled::open("my_db").unwrap();
+
+    db.insert(b"name", b"jacky");
+    assert_eq!(&db.get(b"name").unwrap().unwrap(), b"jacky");
     println!("Hello, world!");
 }
