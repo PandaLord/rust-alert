8ed11829c082bea006e8bc02d4dd485ff3d14e32
PandaLord <330213905@qq.com>
2022-01-05 23:59:16 +0800

    change db to sqlite, add relevant crates

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 pub mod trigger;
 pub mod data;
+pub mod db_config;
 
+use db_config::*;
+use sea_orm::{DbErr, Database};
+use serde_json;
+use tokio::*;
 
-use sled;
-fn main() {
-    let db : sled::Db  = sled::open("my_db").unwrap();
+#[tokio::main]
+async fn main() -> Result<(), DbErr>{
+    let db = Database::connect("sqlite://base.db").await.unwrap();
 
-    db.insert(b"name", b"jacky");
-    assert_eq!(&db.get(b"name").unwrap().unwrap(), b"jacky");
-    println!("Hello, world!");
+    println!("{:?}\n", db);
+    let data: AlertData<&str>= AlertData::new("test");
+    println!("data: {:?}", data);
+    Ok(())
+    // db.insert(b"name", serde_json::to_string(&data).unwrap().as_str()).unwrap();
+    // println!("{:?}", db.get(b"name"));
+    // assert_eq!(&db.get(b"name").unwrap().unwrap(), b"jacky");
+    // println!("Hello, world!");
 }
