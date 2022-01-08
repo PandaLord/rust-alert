74813daec9340a165c865ea902d4e47bc8c66ecf
Jacky Chen <pandalord65@gmail.com>
2022-01-08 13:52:27 +0800

    create table

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,25 @@ pub mod data;
 pub mod alert;
 pub mod entities;
 
-use sea_orm::{DbErr, Database};
+use sea_orm::{DbErr, Database, ConnectionTrait, Schema, DatabaseConnection};
 use entities::*;
 use serde_json;
 use tokio::*;
 use chrono::prelude::*;
 use sea_orm::{entity::*, error::*, DbConn};
 
+
+async fn create_table(db:DbConn) -> Result<DatabaseConnection, DbErr> {
+    let builder = db.get_database_backend();
+    let schema = Schema::new(builder);
+    builder.build(&schema.create_table_from_entity(Alert));
+    Ok(db)
+}
 #[tokio::main]
 async fn main() -> Result<(), DbErr>{
     let db = Database::connect("sqlite://base.db").await.unwrap();
-
-    println!("{:?}\n", db);
+    let db = create_table(db).await.unwrap();
+    // println!("{:?}\n", &db);
 
     let alert_data = alert::ActiveModel {
         alert_message: Set("Test".to_owned()),
